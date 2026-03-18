fb39e59b3f605400944a69161435c301549826db
Mark Malstrom <mark@malstrom.me>
2026-03-17 21:23:17 -0600

    Fade the opacity of visited restaurants

--- app/home.tsx
+++ app/home.tsx
@@ -1,7 +1,10 @@
 import { BookOpenIcon } from "@heroicons/react/24/outline";
 import { Divider, Heading, Button } from "@tailwindcss/ui";
+import { clsx } from "clsx";
 import assert from "node:assert";
 import { getCollection, getEntry, render } from "sprinkles:content";
+
+const VISITED = ["cucina-bella"];
 
 export async function ServerComponent() {
     let restaurants = await getCollection("restaurants");
@@ -24,51 +27,58 @@ async function ListItem({ id }: { id: string }) {
     assert(entry, `Could not find entry for slug: ${id}`);
     let { Content } = await render(entry);
 
-    return (
-        <li className="flex flex-col items-start gap-6 border-black/15 py-6 first-of-type:pt-0! md:flex-row dark:border-white/15">
-            <div className="relative w-full md:w-auto">
-                <div className="aspect-3/2 md:aspect-square block w-full overflow-hidden rounded-lg bg-black/10 md:w-52 dark:bg-white/10">
-                    <img alt="" className="size-full object-cover" src={entry.data.thumbnail} />
-                </div>
-            </div>
+    let hasVisited = VISITED.includes(entry.id);
 
-            <div className="flex w-full flex-col justify-between gap-2 overflow-hidden">
-                <div className="flex flex-col gap-2">
-                    <div className="flex flex-col gap-1">
-                        <h2 className="text-xl font-medium text-black/95 md:truncate dark:text-white/95">
-                            {entry.data.name}
-                        </h2>
+    return (
+        <li className="border-black/15 py-6 first-of-type:pt-0! dark:border-white/15">
+            <div
+                className={clsx("flex flex-col items-start gap-6 md:flex-row", {
+                    "opacity-30": hasVisited,
+                })}
+            >
+                <div className="relative w-full md:w-auto">
+                    <div className="aspect-3/2 md:aspect-square block w-full overflow-hidden rounded-lg bg-black/10 md:w-52 dark:bg-white/10">
+                        <img alt="" className="size-full object-cover" src={entry.data.thumbnail} />
+                    </div>
+                </div>
+                <div className="flex w-full flex-col justify-between gap-2 overflow-hidden">
+                    <div className="flex flex-col gap-2">
+                        <div className="flex flex-col gap-1">
+                            <h2 className="text-xl font-medium text-black/95 md:truncate dark:text-white/95">
+                                {entry.data.name}
+                            </h2>
+                            <div className="text-sm text-black/70 dark:text-white/70">
+                                <span className="inline-block text-wrap">
+                                    {entry.data.cuisine} •{" "}
+                                    <a
+                                        className="underline hover:text-blue-500 dark:hover:text-blue-400"
+                                        href={`https://www.google.com/maps/dir/?api=1&destination=${entry.data.address
+                                            .split(" ")
+                                            .join("+")}`}
+                                        target="_blank"
+                                    >
+                                        {entry.data.address}
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
                         <div className="text-sm text-black/70 dark:text-white/70">
-                            <span className="inline-block text-wrap">
-                                {entry.data.cuisine} •{" "}
-                                <a
-                                    className="underline hover:text-blue-500 dark:hover:text-blue-400"
-                                    href={`https://www.google.com/maps/dir/?api=1&destination=${entry.data.address
-                                        .split(" ")
-                                        .join("+")}`}
-                                    target="_blank"
-                                >
-                                    {entry.data.address}
-                                </a>
-                            </span>
+                            <Content />
                         </div>
                     </div>
-                    <div className="text-sm text-black/70 dark:text-white/70">
-                        <Content />
+                    <div>
+                        {entry.data.menu !== undefined && (
+                            <Button
+                                className="text-blue-500 dark:text-blue-400"
+                                href={entry.data.menu}
+                                plain
+                                target="_blank"
+                            >
+                                Menu
+                                <BookOpenIcon className="stroke-blue-500 dark:stroke-blue-400" />
+                            </Button>
+                        )}
                     </div>
-                </div>
-                <div>
-                    {entry.data.menu !== undefined && (
-                        <Button
-                            className="text-blue-500 dark:text-blue-400"
-                            href={entry.data.menu}
-                            plain
-                            target="_blank"
-                        >
-                            Menu
-                            <BookOpenIcon className="stroke-blue-500 dark:stroke-blue-400" />
-                        </Button>
-                    )}
                 </div>
             </div>
         </li>
